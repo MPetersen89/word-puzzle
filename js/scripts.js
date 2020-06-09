e7ec5b1db95e0e034fb15323185422e599723a95
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-09 11:59:57 -0700

    add the beginnings of a for loop with jquery grabbing our user input
    Co-Authored-by: Michael Petersen <michael_petersen_email@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
   $("div.form-group").submit(function(event) {
     event.preventDefault();
+    let inputItem = $("#input").val();
+    let inputsArray = inputItem.split("");
     
+      for (let input=0; input <= inputsArray.length-1; input+=1);
+
+      
+    if (letter === "a" || "e" || "i" || "o" || "u") {
+      $("letter").replaceValue("-");
+    }
+      // else if (letter === "e");
+      // else if (letter === "i");
+      // else if (letter === "o");
+      // else if (letter === "u");
   });
   $("button#inputButton").click(function()  {
     $("div.form-group").hide();
